eb248f55b5793d27b92b5aa34aa9db743405a095
NARENDRUNI SANDEEP <139003520+Narendruni-Sandeep@users.noreply.github.com>
2024-04-17 09:18:18 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,12 @@
+    function clear() {
+        // Get a reference to the imp div
+        var impDiv = document.querySelector('.imp');
+
+        // Clear existing contents of the div
+        while (impDiv.firstChild) {
+            impDiv.removeChild(impDiv.firstChild);
+        }
+
+        // Create a new label element
+      
+    }
